e680411ccf4c12a9d4ba3cf180e95ebc5bfae4b4
Roman Tarassenko <tarassenko@me.com>
2022-10-29 10:46:21 +0300

    Добавлены 2 модели: 1) Product 2) Review - m2m Product + auth.User

--- reviewer_app/models.py
+++ reviewer_app/models.py
@@ -1,3 +1,64 @@
+from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Product(models.Model):
+    class Categories(models.TextChoices):
+        FOOD = 'FD', _('Food')
+        TOYS = 'TS', _('Toys')
+        OTHER = 'OT', _('Other')
+        HOME = 'HM', _('Home')
+
+    name = models.CharField(
+        max_length=100,
+        null=False,
+        blank=False
+    )
+    category = models.CharField(
+        max_length=2,
+        null=False,
+        blank=False,
+        choices=Categories.choices
+    )
+    description = models.CharField(
+        max_length=300,
+        null=True,
+        blank=True
+    )
+    picture = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to="Profile picture"
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Review(models.Model):
+    author = models.ForeignKey(
+        to=get_user_model(),
+        related_name="reviews",
+        on_delete=models.CASCADE,
+        verbose_name="Author"
+    )
+    product = models.ForeignKey(
+        to="reviewer_app.Product",
+        related_name="reviews",
+        on_delete=models.CASCADE,
+        verbose_name="Product"
+    )
+    review_text = models.TextField(
+        null=False,
+        blank=False,
+    )
+    rate = models.PositiveIntegerField(
+        validators=(
+            MaxValueValidator(5),
+            MinValueValidator(1))
+    )
+
+    def __str__(self):
+        return f"{self.product} - {self.rate}"
